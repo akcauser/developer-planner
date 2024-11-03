cf86d21742451a541dc800b2ae836a26a5a2ea89
Ertugrul AKCA <ertugrulakcatp@gmail.com>
2024-11-03 17:34:03 +0300

    bootstrap added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,17 +6,20 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Developer Planner</title>
-    <style>
-        table, th, td {
-            border: 1px solid black;
-            border-collapse: collapse;
-        }
-    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     @foreach($solutions as $key => $solution)
-    <h1>{{ $key }}</h1>
-    <table>
+    <h1></h1>
+    <table class="table table-striped table-hover text-center table-bordered">
+        <tr>
+            <th colspan="2">
+                {{ $key }} <br>
+                <strong>Elapsed Time:</strong> {{ $solution['elapsedTime'] }} hours 
+                --
+                <strong>Elapsed Time:</strong> {{ ceil($solution['elapsedTime'] / 45)  }} week(s)
+            </th>
+        </tr>
         <tr>
             <th>Developer</th>
             <th>Tasks</th>
@@ -27,12 +30,7 @@
                 <td>{{ implode(',', $developer['tasks']) }}</td>
             </tr>
         @endforeach
-
     </table>
-    <br>
-    <b>Elapsed Time:</b> {{ $solution['elapsedTime'] }} hours
-    <br>
-    <b>Elapsed Time:</b> {{ ceil($solution['elapsedTime'] / 45)  }} week(s)
     @endforeach
 </body>
 </html>
